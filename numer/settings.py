66819369a9e705c43db85ca43fd46afa14cf04e3
Ketib Oldiais <numerstack@gmail.com>
2021-09-24 14:56:02 -0500

    add static root

--- numer/settings.py
+++ numer/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/images/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
